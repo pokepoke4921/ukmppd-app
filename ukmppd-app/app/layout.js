3e48f20e3fa5ff6a1065f8f6069cf050a2baa7d7
pokepoke4921 <pokepoke4921@gmail.com>
2026-03-20 18:04:24 +0700

    Delete ukmppd-app directory

--- ukmppd-app/app/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = { title: "UKMPPD Study App" };
-export default function RootLayout({ children }) {
-  return (
-    <html lang="id">
-      <body style={{ margin: 0, padding: "1rem", background: "#fff" }}>{children}</body>
-    </html>
-  );
-}
